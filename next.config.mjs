dacc11b2f2c68b296dd4293c5fa7e22139b6b898
Luminaterminal <admin@luminaterminal.com>
2025-08-12 01:31:05 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true },
-  // Nëse s'po përdor CDN për Next/Image
-  images: { unoptimized: true }
+  reactStrictMode: true,
+  output: 'standalone',
 };
 
 export default nextConfig;
