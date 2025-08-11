1ea024c5a7ee5dd4b57ffd246990ad5c305491e9
Luminaterminal <admin@luminaterminal.com>
2025-08-12 01:32:11 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,8 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/'
-    },
+    rules: [{ userAgent: '*', allow: '/' }],
     sitemap: 'https://luminaterminal.com/sitemap.xml',
-    host: 'https://luminaterminal.com'
+    host: 'https://luminaterminal.com',
   };
 }
